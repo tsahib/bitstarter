d846bd883abd6b2ae1cba6d2c9043a147fd0a3db
Tsahib <bengigi@gmail.com>
2013-07-09 20:43:00 +0000

    update test

--- web.js
+++ web.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var str = fs.readFileSync('/index.html');
   response.send('Hello World2!');
 });
 
