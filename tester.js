dd293e287aad8b2991c3d56739f03d31b801b8e8
Tsahib <bengigi@gmail.com>
2013-08-08 20:05:59 +0000

    done some changes

---
+++ tester.js
@@ -0,0 +1,15 @@
+var getReview = function (movie) {
+    switch (movie)
+    {
+        case "Matrix":
+            return "good trip out";
+        case "Welcome to America":
+            return "Amjad's favorite";
+        case "Remember the Titans":
+            return "love the sports";
+        case "Why do I look like I'm 12?":
+            return "The Ryan and Zach story";
+    }
+};
+
+
